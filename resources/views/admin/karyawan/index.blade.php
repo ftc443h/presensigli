d99b84b44f584bf6766eb07329f70973274bdb14
ftc443h <fatchanmuhammad.hak9im@gmail.com>
2025-06-08 01:31:58 +0700

    feature/karyawan

---
+++ resources/views/admin/karyawan/index.blade.php
@@ -0,0 +1,34 @@
+@extends('admin.layouts.index')
+@section('content')
+
+<!-- BEGIN PAGE HEADER -->
+<div class="page-header d-print-none">
+    <div class="container-xl">
+        <div class="row g-2 align-items-center">
+            <div class="col">
+                <!-- Page pre-title -->
+                <h2 class="page-title"> <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler.io/icons/icon/home -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-users-group">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                            <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1" />
+                            <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                            <path d="M17 10h2a2 2 0 0 1 2 2v1" />
+                            <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                            <path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
+                        </svg></span> Karyawan</h2>
+            </div>
+            <!-- Page title actions -->
+        </div>
+    </div>
+</div>
+<!-- END PAGE HEADER -->
+<!-- BEGIN PAGE BODY -->
+<div class="page-body">
+    <div class="container-xl">
+        <div class="row row-deck row-cards">
+
+        </div>
+    </div>
+</div>
+@endsection
